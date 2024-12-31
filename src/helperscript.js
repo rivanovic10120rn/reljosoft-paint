f2f9cf8a503996baf3d8d0902e2bd5abfb437fa3
Reljadin <rivanovic10120rn@raf.rs>
2024-12-31 05:57:16 +0100

    Nastavak rada na scriptu.

--- src/helperscript.js
+++ src/helperscript.js
@@ -25,10 +25,89 @@ let canvasSnapshot = null;
 const initCanvas = () => {
   document.documentElement.style.setProperty('--doc-height', `${window.innerHeight}px`);
   const dpr = window.devicePixelRatio || 1;
-  const canvasArea = canvas.getBoundingClientRect();
-  canvas.width = canvasArea.width * dpr;
-  canvas.height = canvasArea.height * dpr;
+  const canvasRect = canvas.getBoundingClientRect();
+  canvas.width = canvasRect.width * dpr;
+  canvas.height = canvasRect.height * dpr;
   context.scale(dpr, dpr);
 }
 
 // Canvas reset
+const resetCanvas = () => {
+  initCanvas();
+  context.fillStyle = "#fff";
+  context.fillRect(0,0, canvas.width, canvas.height);
+  drawingHistory.push(localStorage.getItem("savedDrawing") || canvas.toDataURL());
+}
+
+const getImageSize = (image) => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  const canvasRect = canvas.getBoundingClientRect();
+  const aspectRatio = image.width / image.height;
+  const newWidth = canvasRect.width;
+  const newHeight = newWidth / aspectRatio;
+  return { newWidth, newHeight };
+}
+
+// Ucitavanje crteza iz local storagea na canvas
+const loadLocalStorageDrawing = () => {
+  const savedDrawing = localStorage.getItem("savedDrawing");
+  if (!savedDrawing) return;
+
+  const image = new Image();
+  image.src = savedDrawing;
+  image.onload = () => {
+    const { newWidth, newHeight } = getImageSize(image);
+    context.drawImage(image, 0, 0, newWidth, newHeight);
+  }
+}
+
+// Cuvanje crteza na local storage
+const saveDrawingToLocalStorage = () => {
+  const canvasDrawing = canvas.toDataURL();
+  localStorage.setItem("savedDrawing", canvasDrawing);
+}
+
+// Cuvanje stanje crteza po varijablama
+const saveDrawingState = () => {
+  if (currentStep < drawingHistory.length - 1) {
+    drawingHistory = drawingHistory.slice(0, currentStep + 1);
+  }
+  currentStep++;
+  drawingHistory.push(canvas.toDataURL());
+  redoHistory = [];
+  saveDrawingToLocalStorage();
+}
+
+// Dvojna funkcionalnost za undo i redo
+const activateUndoRedo = (selectedButton) => {
+  if (selectedButton.id === "undo" && currentStep > 0) {
+    currentStep--;
+    redoHistory.push(drawingHistory[currentStep + 1]);
+  } else if (selectedButton.id === "redo" && redoHistory.length > 0) {
+    currentStep++;
+    drawingHistory.push(redoHistory.pop());
+  } else {
+    return;
+  }
+
+  const image = new Image();
+  image.src = drawingHistory[currentStep];
+  image.onload = () => {
+    const { newWidth, newHeight } =getImageSize(image);
+    context.drawImage(image, 0, 0, newWidth, newHeight);
+    saveDrawingToLocalStorage();
+  }
+}
+
+// Uzimanje trenutne lokacije kursora
+const cursorLocation = (e) => {
+  let x = ("ontouchstart" in window ? e.touches?.[0]?.pageX : e.pageX) - canvas.offsetLeft;
+  let y = ("ontouchstart" in window ? e.touches?.[0]?.pageY : e.pageY) - canvas.offsetTop;
+  return { x, y };
+}
+
+// Crtanje Linije
+
+// Crtanje pravougaonika
+
+//
